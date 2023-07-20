63efcdb22744f9992a564f6104a1662cc897c737
minechoo <choominhae@gmail.com>
2023-07-20 11:57:04 +0900

    주석

--- client/src/community/List.js
+++ client/src/community/List.js
@@ -12,8 +12,10 @@ function List() {
 	}, []);
 	return (
 		<Layout name={'List'}>
+			{/* map의 idx는 권장사항 아님 */}
 			{Posts.map((post) => {
 				return (
+					// 몽고디비에 있는 고유값 _id
 					<article key={post._id}>
 						<h2>{post.title}</h2>
 					</article>

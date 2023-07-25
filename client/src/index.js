47d830761472b5fa02b4c2060f8685c4b6155b5c
minechoo <choominhae@gmail.com>
2023-07-25 11:33:22 +0900

    login

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import userSlice from './redux/userSlice';
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+	reducer: {
+		user: userSlice,
+	},
+});
 
 ReactDOM.render(
 	<BrowserRouter>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</BrowserRouter>,
 	document.getElementById('root')
 );
---
+++ client/src/redux/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+	name: 'user',
+	initialState: { displayName: '', uid: '' },
+	reducers: {
+		loginUser: (state, action) => {
+			state.displayName = action.payload.displayName;
+			state.uid = action.payload.uid;
+		},
+		logoutUser: (state) => {
+			state.displayName = '';
+			state.uid = '';
+		},
+	},
+});
+
+export const { loginUser, logoutUser } = userSlice.actions;
+export default userSlice.reducer;
